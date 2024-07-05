d0fb6e06346e4633a44d13dcd2ad473f1f388a49
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-07-05 19:38:04 +0200

    Added console logs and logic to test singleton pattern

--- index.js
+++ index.js
@@ -22,23 +22,13 @@ branchSecondary = new BankBranch({
   address: "56 javascript Street",
 });
 
-// 5. In the usage section:
-//    - Create instances of the `BankBranch` class, such as `branchA` and `branchB`, with different branch information.
-//    - Use the `getBranchInfo` method to retrieve branch information from the instances.
-//    - Verify that `branchA` and `branchB` are both referring to the same instance by comparing them using `===`.
+console.log(branchMain.getBranchInfo());
+console.log(branchSecondary.getBranchInfo());
 
-// This pseudo-code provides a step-by-step explanation of how to implement the Singleton Pattern for managing a single instance of the `BankBranch` class throughout the application, ensuring that multiple instances refer to the same object.
-
-// NOTES:
-// This example demonstrates the Singleton pattern by creating a single instance of the `BankBranch` class.
-// Even if we attempt to create another instance of the branch, we receive the original instance,
-// ensuring that there is only one set of branch information throughout the application.
-
-// - We define a `BankBranch` class that represents bank branch information.
-// - We use the Singleton pattern to ensure that there is only one instance of the `BankBranch` class.
-// - The `bankBranchInstance` variable is used to hold the single instance of the class.
-// - When a new `BankBranch` instance is created, it checks if `bankBranchInstance` already exists.
-//   If it does, it returns the existing instance; otherwise, it creates a new one.
-// - The `getBranchInfo` method allows us to retrieve the branch information from the singleton instance.
-// - We demonstrate the Singleton pattern by creating two instances (`branchA` and `branchB`) and show that they both point to the same instance,
-//   ensuring that there is only one set of branch information throughout the application.
+if (branchMain.getBranchInfo() === branchSecondary.getBranchInfo()) {
+  console.log(
+    "Both the Main branch and the Secondary Branch point to the same instance"
+  );
+} else {
+  console.log("The branches do not point to the same instance");
+}
